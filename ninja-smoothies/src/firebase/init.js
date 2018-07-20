fbab8ce15e19b8e3aa7b42dcbf0ef7bbbb11b0e5
Jorgeural <jorgeural88@gmail.com>
2018-07-20 14:12:39 +0200

    Firebase init

--- ninja-smoothies/src/firebase/init.js
+++ ninja-smoothies/src/firebase/init.js
@@ -3,12 +3,12 @@ import firestore from 'firebase/firestore'
 
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyChAzJglXh0T6CY7_wM5Kd5OILX3UXIcic",
-    authDomain: "ninja-smoothies-989f4.firebaseapp.com",
-    databaseURL: "https://ninja-smoothies-989f4.firebaseio.com",
-    projectId: "ninja-smoothies-989f4",
-    storageBucket: "ninja-smoothies-989f4.appspot.com",
-    messagingSenderId: "183584290462"
+    // apiKey: "Your api key",
+    // authDomain: authDomain,
+    // databaseURL: databaseURL,
+    // projectId: projectId,
+    // storageBucket: storageBucket,
+    // messagingSenderId: messagingSenderId
   };
   const firebaseApp = firebase.initializeApp(config);
   firebaseApp.firestore().settings({timestampsInSnapshots: true})
