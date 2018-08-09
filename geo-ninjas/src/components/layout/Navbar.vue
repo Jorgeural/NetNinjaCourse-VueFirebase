290c42f7315275bbfd404840dd96f18e5cdb6e60
Jorgeural <jorgeural88@gmail.com>
2018-08-09 08:40:29 +0200

    nav guards & nav btns

--- geo-ninjas/src/components/layout/Navbar.vue
+++ geo-ninjas/src/components/layout/Navbar.vue
@@ -4,9 +4,9 @@
       <div class="container">
         <router-link :to="{name: 'GMap'}">GeoNinjas!</router-link>
         <ul class="right">
-          <li><router-link :to="{name: 'Signup'}"> Sign Up</router-link></li>
-          <li><router-link :to="{name: 'Login'}"> Log in</router-link></li>
-          <li><a @click="logout">Logout</a></li>
+          <li v-if="!user"><router-link :to="{name: 'Signup'}"> Sign Up</router-link></li>
+          <li v-if="!user"><router-link :to="{name: 'Login'}"> Log in</router-link></li>
+          <li v-if="user"><a @click="logout">Logout</a></li>
         </ul>
       </div>
     </nav>
@@ -19,7 +19,7 @@ export default {
   name: 'Navbar',
   data () {
     return {
-
+      user: null
     }
   },
   methods:{
@@ -28,6 +28,16 @@ export default {
         this.$router.push({name: 'Login'})
       })
     }
+  },
+  created () {
+    
+    firebase.auth().onAuthStateChanged((user) => {
+      if(user) {
+        this.user = user
+      } else {
+        this.user = null
+      }
+    })
   }
 }
 </script>
